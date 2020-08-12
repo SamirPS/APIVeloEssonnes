eeada3ac62071a3f6cccd35b1f439613940746ca
unknown <akariohsamir@yahoo.com>
2020-08-12 21:13:19 +0200

    Lambda fonction et map

--- api.py
+++ api.py
@@ -10,23 +10,24 @@ app = FastAPI()
 f = open('a.json',) 
 data = json.load(f) 
 
-g=[tuple(data[i]["fields"]["geo_point_2d"]) for i in range(len(data))]
+g=[tuple(i["fields"]["geo_point_2d"]) for i in data]
 
 @app.get("/")
-async def Redirect():
-    response = RedirectResponse(url='/docs')
-    return response
+async def Redirect(): 
+    return RedirectResponse(url='/docs')
 
 @app.get("/proche/{rue}/{ville}/{pays}")
 async def proche(rue: str, ville:str,pays:str):
 
     geolocator = Nominatim(user_agent="myGeocoder")
     location = geolocator.geocode(f"{rue},{ville},{pays}")
-    if location==None:
+    if location is None:
        
         return {"Probleme":"On a pas trouvé l'adresse "}
-        
-    distance=[geodesic((location.latitude, location.longitude), g[i]).miles for i in range(len(g))]
+
+    
+    distance=list(map(lambda a: geodesic((location.latitude, location.longitude), a).miles,g))
+
     location = geolocator.reverse(g[distance.index(min(distance))])
 
     return {"Position": location.address}
