768dee84b957327ddf6a38419dda859251243abd
unknown <akariohsamir@yahoo.com>
2020-08-12 17:55:27 +0200

    URL change

--- api.py
+++ api.py
@@ -1,6 +1,7 @@
 import json 
 from typing import Optional
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from starlette.responses import RedirectResponse
 from geopy.geocoders import Nominatim
 from geopy.distance import geodesic
 
@@ -11,9 +12,13 @@ data = json.load(f)
 
 g=[tuple(data[i]["fields"]["geo_point_2d"]) for i in range(len(data))]
 
+@app.get("/")
+async def Redirect():
+    response = RedirectResponse(url='/docs')
+    return response
 
 @app.get("/proche/{rue}/{ville}/{pays}")
-async def read_item(rue: str, ville:str,pays:str):
+async def proche(rue: str, ville:str,pays:str):
 
     geolocator = Nominatim(user_agent="myGeocoder")
     location = geolocator.geocode(f"{rue},{ville},{pays}")
